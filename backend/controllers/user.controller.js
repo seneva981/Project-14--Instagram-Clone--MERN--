720be180c0a69d460eaa72f8b2ca83b28c54edce
Kumar Anuj <seneva.981@gmail.com>
2025-05-06 23:02:28 +0530

    feat: add user login route

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { validationResult } from "express-validator";
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res) => {
   try {
@@ -41,3 +42,51 @@ export const registerUser = async (req, res) => {
     console.log(error);
   }
 };
+
+export const loginUser = async (req, res) => {
+  try {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      return res.status(400).json({ errors: error.array() });
+    }
+    const { email, username, password } = req.body;
+    if ((!email && !username) || !password) {
+      return res.status(401).json({
+        message: "Either email, username or password is missing",
+        success: false,
+      });
+    }
+    const user = await User.findOne({ $or: [{email},{username}] });
+    if (!user) {
+      return res.status(401).json({
+        message: "User not found with this email or username",
+        success: false,
+      });
+    }
+    const isPasswordMatched = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatched) {
+      return res.status(401).json({
+        message: "Incorrect password",
+        success: false,
+      });
+    }
+    const userCopy = {
+      _id: user.id,
+      username: user.username,
+      email: user.email,
+      profilePicture: user.profilePicture,
+      bio: user.bio,
+      followers: user.followers,
+      following: user.following,
+      posts: user.posts
+    }
+    const token = await jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+    return res.cookie("token", token, { httpOnly: true, secure: true, sameSite: "none", maxAge: 1 * 24 * 60 * 60 * 1000 }).status(200).json({
+      message: "Login successfully",
+      success: true,
+      userCopy,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
